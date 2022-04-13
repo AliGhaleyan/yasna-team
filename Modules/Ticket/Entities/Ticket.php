9024b1002ad0c75bc51f91dc59e3754fa34f511e
Ali ghaleyan <alighalehban1379@gmail.com>
2022-04-13 13:46:17 +0430

    Add ticket migration and fields

--- Modules/Ticket/Entities/Ticket.php
+++ Modules/Ticket/Entities/Ticket.php
@@ -2,12 +2,28 @@
 
 namespace Modules\Ticket\Entities;
 
-use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 
+/**
+ * Class Ticket
+ * @package Modules\Ticket\Entities
+ *
+ * @property $id
+ * @property $title
+ * @property $description
+ * @property $status
+ * @property $code
+ */
 class Ticket extends Model
 {
     use HasFactory;
 
-    protected $fillable = [];
+    protected $fillable = [
+        "id",
+        "title",
+        "description",
+        "status",
+        "code",
+    ];
 }
