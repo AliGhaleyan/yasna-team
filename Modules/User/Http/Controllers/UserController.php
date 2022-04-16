43b27e1c6b5a4639d88b9b52a518bee2770e8867
ghaleyan <ali.ghaleyan@gmail.com>
2022-04-16 20:50:20 +0430

    Create register api

--- Modules/User/Http/Controllers/UserController.php
+++ Modules/User/Http/Controllers/UserController.php
@@ -5,6 +5,7 @@ namespace Modules\User\Http\Controllers;
 use Illuminate\Http\Resources\Json\ResourceCollection;
 use Illuminate\Routing\Controller;
 use Modules\User\Entities\User;
+use Modules\User\Http\Requests\RegisterRequest;
 use Modules\User\Repositories\UserRepository;
 use Modules\User\Transformers\UserCollection;
 
@@ -20,5 +21,14 @@ class UserController extends Controller
     public function index(): UserCollection
     {
         return new UserCollection($this->repository->paginate());
+    }
+
+    public function store(RegisterRequest $request)
+    {
+        $data = $request->validated();
+        $data["password"] = bcrypt($data["password"]);
+        User::query()->create($data);
+
+        return response(["message" => "User registered successfully"]);
     }
 }
--- Modules/User/Routes/api.php
+++ Modules/User/Routes/api.php
@@ -4,4 +4,5 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware("auth:api")->prefix("user")->group(function () {
     Route::get("/", "UserController@index")->middleware("view.users");
+    Route::post("/", "UserController@store");
 });
